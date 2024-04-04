08b1a482dde6a065238b95db2373113c4278e54d
Şeyma Bilgin <149247597+seymabilgin@users.noreply.github.com>
2024-04-04 22:42:47 +0300

    v2

---
+++ src/test/java/Tests/Test01Test.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static java.lang.System.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test01Test {
+    @BeforeEach
+    void setUp() {
+//v2 degisikligi
+    }
+}
